a4a9d6275e597badccd653ddadae27e6d2bdef3b
Georgios Loulakis <georgiosloulakis@gmail.com>
2023-04-15 21:21:15 +0300

    change country

--- userDefaults.swift
+++ userDefaults.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-let COUNTRY = "us"
+let COUNTRY = "gb"
 private let apiKey = "4ed2a8ca15ed415c8503d611780f19b5"
 let BASE_URL = "https://newsapi.org/v2/top-headlines?country=\(COUNTRY)&apiKey=\(apiKey)"
 
